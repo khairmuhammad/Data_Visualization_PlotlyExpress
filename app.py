ff62650c5f4f21ae2ce3910532e7a5246660c72e
unknown <khairmuhammadmemon@gmail.com>
2020-06-29 20:12:31 +0500

    project

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import json
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -8,15 +7,8 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__)
-
 server = app.server
-
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 
